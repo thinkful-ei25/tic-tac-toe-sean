b3f536713def3acb361cacec3322be1fafcc74e2
Sean Nealon <signalflowsean@gmail.com>
2018-10-01 17:05:04 -0400

    generating html to render board

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,52 @@
 'use strict';
+/*eslint-env jquery*/
 
-const app = (function(){ 
-  function generateItem(){}; 
-  function generateAllItems(){}; 
-  function render(){}; 
-  function onGameStartClick(){}; 
-  function onCellClick(){}; 
+const app = (function(){
+  function generateCell(index){ 
+    return `  
+    <div class="cell" id="${index}">
+        <p>&nbsp;</p>
+    </div>`; 
+  }
+  function generateRow(){
+    let rowLength = 3; 
+    let row = []; 
+    row.push(' <div class="row">'); 
+    for (let i = 0; i < rowLength; i++){ 
+      row.push(generateCell(i)); 
+    }
+    row.push('</div>'); 
+    return row.join(' '); 
+  } 
 
-  function bindEventListeners(){}; 
+  function generateBoard(){ 
+    let numRows = 3; 
+    let board = []; 
+    for (let i = 0; i < numRows; i++){ 
+      board.push(generateRow()); 
+    }
+    return board; 
+  }
 
+  function render(){
+    const gameBoardString = generateBoard(); 
+    // console.log(gameBoardString); 
+    $('.board').html(gameBoardString); 
+  }
+
+  function handleOnGameStartClick(){
+    render(); 
+  }
+  function handleOnCellClick(){}
+
+  function bindEventListeners(){
+    handleOnCellClick(); 
+    handleOnGameStartClick(); 
+  }
+
+  return {render, bindEventListeners}; 
 }()); 
-
+app.render(); 
 
 // State modification functions
 
